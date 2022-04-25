91e73d737f64f46380dac8a73900785c944dd664
Peter Cheng <pcheng17@gmail.com>
2022-04-25 12:00:57 -0700

    Modified layer

--- keyboards/dz60/keymaps/kcwebers/keymap.c
+++ keyboards/dz60/keymaps/kcwebers/keymap.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+// The dz60 keyboard uses a 5 x 15 matrix,
+// but the LAYOUT_60_ansi wrapper macro only requires:
+//     Row 1: 14 keys
+//     Row 2: 14 keys
+//     Row 3: 13 keys
+//     Row 4: 12 keys
+//     Row 5: 8 keys
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_60_ansi(
         KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,        KC_EQL,  KC_BSPC,
@@ -9,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, LT(1, KC_LEFT), KC_DOWN, RCTL_T(KC_RIGHT)
     ),
 
-    LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
+    LAYOUT_60_ansi(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, ______, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, RESET,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______
     )
 };
